3077c2c298d24fe22db10b8f5f5c5744d00491ec
Leo Jay Magistrado <ljballanomagistrado020@gmail.com>
2024-05-05 18:25:03 +0800

    finished task

---
+++ hello-world/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello, World!</title>
+    <style>
+        h1{
+            display : flex;
+            justify-content: center;
+            align-items: center;
+            background-color: red;
+            color: cyan;
+            height: 50vh;
+        }
+        h1:hover{
+            background-color: pink;
+            color: cyan;
+        }
+    </style>
+</head>
+<body>
+        <h1><?php echo 'Hello, World!'; ?></h1>
+</body>
+</html>
